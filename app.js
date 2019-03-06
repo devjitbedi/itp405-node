2e5d8f22968c5a0510cb732a405fea93e84eac0b
Devjit Bedi <devjitbedi@Devjits-MacBook-Pro.local>
2019-03-05 23:48:01 -0800

    done with lab

--- app.js
+++ app.js
@@ -16,6 +16,77 @@ app.get('/api/genres', function(request, response) {
   });
 });
 
+app.get('/api/artists', function(request, response) {
+
+  let filter = request.query.filter;
+
+  let connection = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: 'chinook.db'
+    }
+  });
+
+  if (filter) {
+
+  connection
+  .select()
+  .from('artists')
+  .where('Name', "like", '%' + filter + '%')
+  .then((artists) => {
+
+  	if (artists.length != 0) {
+    
+    var reformattedArtists = artists.map(obj =>{ 
+   var rObj = {};
+   rObj["id"] = obj.ArtistId;
+   rObj["name"] = obj.Name;
+   return rObj;
+	});
+   
+    console.log(request.query);
+    response.json(reformattedArtists);
+
+	} else {
+        response.status(404).json({
+          error: `Artist with ${filter} not found`
+        });
+      }
+
+  });
+
+}
+
+else {
+
+	connection
+  .select()
+  .from('artists')
+  .then((artists) => {
+
+  	if (artists) {
+    
+    var reformattedArtists = artists.map(obj =>{ 
+   var rObj = {};
+   rObj["id"] = obj.ArtistId;
+   rObj["name"] = obj.Name;
+   return rObj;
+	});
+   
+    console.log(request.query);
+    response.json(reformattedArtists);
+
+	}
+
+  });
+
+}
+
+});
+
+
+
+
 app.get('/api/genres/:id', function(request, response) {
   let id = request.params.id;
 
@@ -23,7 +94,7 @@ app.get('/api/genres/:id', function(request, response) {
     client: 'sqlite3',
     connection: {
       filename: 'chinook.db'
-      
+
     }
   });
 
@@ -43,6 +114,8 @@ app.get('/api/genres/:id', function(request, response) {
     });
 });
 
+
+  
 
 app.listen(process.env.PORT || 8000);
 
